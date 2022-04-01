cc946454c7dac0c6f787d9c731dba5e2e85a9be6
Dominik B <domburkart@gmail.com>
2022-04-01 13:41:38 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#111111',
+      'light-grey': '#EAEAEA',
+      'acid': '#CEFF00',
+      'purple': '#9C50FF'
+    },
+    fontFamily: {
+      'body': ['Poppins']
+    },
+    fontSize: {
+      'xs': '.618rem',
+      'base': '1rem',
+      'lg': '1.618rem',
+      'xl': '2.618rem',
+      '2xl': '4.236rem',
+      '3xl': '6.845rem',
+      '4xl': '11.089rem',
+    },
+    extend: {}
+  },
+  plugins: [""]
+}
